cf0679ac1817e09f570b1317116858df4c2614da
Syed Rafay <rafay_syed1@yahoo.com>
2017-06-13 09:10:24 +0500

    Added icon resources to the project

--- Achievements/Form1.Designer.cs
+++ Achievements/Form1.Designer.cs
@@ -239,8 +239,8 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(730, 559);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.heliCount);

eb6583a2b408afe09f4b220cdf3285edc84b4d33
Syed Rafay <rafay_syed1@yahoo.com>
2017-06-13 07:19:04 +0500

    Refactored the code and added safety conditions and messages

--- Achievements/Form1.cs
+++ Achievements/Form1.cs
@@ -24,92 +24,100 @@ namespace Achievements
             string fileName = "";
 
             if (membersName.TextLength == 0)
-                MessageBox.Show("Please enter the members name in the field", "Error",
+                MessageBox.Show("Please enter the members name in the field", "ERROR!!",
                  MessageBoxButtons.OK, MessageBoxIcon.Error);
             else
             {
                 fileName = membersName.Text;
 
                 if (achievementType.CheckedIndices.Count == 0)
-                    MessageBox.Show("Please select the achievement type for the user", "Error",
+                    MessageBox.Show("Please select the achievement type for the user", "ERROR!!",
                      MessageBoxButtons.OK, MessageBoxIcon.Error);
                 else
                 {
                     if (achievementType.CheckedIndices.Count > 1)
-                        MessageBox.Show("You can only select one achievement at a time", "Error",
+                        MessageBox.Show("You can only select one achievement at a time", "ERROR!!",
                          MessageBoxButtons.OK, MessageBoxIcon.Error);
                     else
                     {
-                        if (achievementType.GetItemCheckState(0) == CheckState.Checked)
-                            fileName += @"\Chainsaw.txt";
-                        else if (achievementType.GetItemCheckState(1) == CheckState.Checked)
-                            fileName += @"\Sniper.txt";
-                        else if (achievementType.GetItemCheckState(2) == CheckState.Checked)
-                            fileName += @"\Bomb.txt";
-                        else if (achievementType.GetItemCheckState(3) == CheckState.Checked)
-                            fileName += @"\DragMW.txt";
-                        else if (achievementType.GetItemCheckState(4) == CheckState.Checked)
-                            fileName += @"\HeliDrag.txt";
-
-                        if (!File.Exists(fileName))
-                        {
-                            string buffer = "";
-                            int numOfLines = suspectsNames.Lines.Length;
-                            FileInfo file = new FileInfo(fileName);
-                            file.Directory.Create(); // If the directory already exists, this method does nothing.
-                            for (int i = 0; i < numOfLines; ++i)
-                            {
-                                buffer += suspectsNames.Lines[i];
-                                buffer += Environment.NewLine;
-                            }
-                            File.WriteAllText(fileName, buffer);
-                        }
+                        if (suspectsNames.Lines.Length == 0)
+                                MessageBox.Show("Please enter the suspect names in the text box below", "ERROR! SUSPECTS NAME LIST IS EMPTY",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                         else
                         {
-                            string lines = "", toAppend = "", repeated = "";
-                            int suspectsInFile = 0;
-                            using (StreamReader sr = new StreamReader(fileName))
+                            if (achievementType.GetItemCheckState(0) == CheckState.Checked)
+                                fileName += @"\Chainsaw.txt";
+                            else if (achievementType.GetItemCheckState(1) == CheckState.Checked)
+                                fileName += @"\Sniper.txt";
+                            else if (achievementType.GetItemCheckState(2) == CheckState.Checked)
+                                fileName += @"\Bomb.txt";
+                            else if (achievementType.GetItemCheckState(3) == CheckState.Checked)
+                                fileName += @"\DragMW.txt";
+                            else if (achievementType.GetItemCheckState(4) == CheckState.Checked)
+                                fileName += @"\HeliDrag.txt";
+
+                            if (!File.Exists(fileName))
                             {
-                                string temp;
-                                // Read and display lines from the file until 
-                                // the end of the file is reached. 
-                                while ((temp=sr.ReadLine()) != null)
+                                string buffer = "";
+                                int numOfLines = suspectsNames.Lines.Length;
+                                FileInfo file = new FileInfo(fileName);
+                                file.Directory.Create(); // If the directory already exists, this method does nothing.
+                                for (int i = 0; i < numOfLines; ++i)
                                 {
-                                    lines += temp;
-                                    lines += Environment.NewLine;
-                                    ++suspectsInFile;
+                                    buffer += suspectsNames.Lines[i];
+                                    buffer += Environment.NewLine;
                                 }
+                                File.WriteAllText(fileName, buffer);
+                                MessageBox.Show("Created a new file '" + fileName + "' and added the following names to it : " +
+                                    Environment.NewLine + buffer, "Succesfully created a new file!");
                             }
-                            string[] newLines = lines.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-                            int numOfSuspects = suspectsNames.Lines.Count();
-                            bool found=false;
-
-                            for (int i = 0; i < numOfSuspects; ++i)
+                            else
                             {
-                                for (int j = 0; j < suspectsInFile; ++j)
+                                string lines = "", toAppend = "", repeated = "";
+                                int suspectsInFile = 0;
+                                using (StreamReader sr = new StreamReader(fileName))
                                 {
-                                    if (suspectsNames.Lines[i] == newLines[j])
-                                        found = true;
+                                    string temp;
+                                    // Read and display lines from the file until 
+                                    // the end of the file is reached. 
+                                    while ((temp = sr.ReadLine()) != null)
+                                    {
+                                        lines += temp;
+                                        lines += Environment.NewLine;
+                                        ++suspectsInFile;
+                                    }
                                 }
-                                if (!found)
+                                string[] newLines = lines.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                                int numOfSuspects = suspectsNames.Lines.Count();
+                                bool found = false;
+
+                                for (int i = 0; i < numOfSuspects; ++i)
                                 {
-                                    // will be added to the file
-                                    toAppend += suspectsNames.Lines[i];
-                                    toAppend += Environment.NewLine;
-                                }
-                                else
-                                {
-                                    // they will be discarded
-                                    repeated += suspectsNames.Lines[i];
-                                    repeated += Environment.NewLine;
+                                    for (int j = 0; j < suspectsInFile; ++j)
+                                    {
+                                        if (suspectsNames.Lines[i] == newLines[j])
+                                            found = true;
+                                    }
+                                    if (!found)
+                                    {
+                                        // will be added to the file
+                                        toAppend += suspectsNames.Lines[i];
+                                        toAppend += Environment.NewLine;
+                                    }
+                                    else
+                                    {
+                                        // they will be discarded
+                                        repeated += suspectsNames.Lines[i];
+                                        repeated += Environment.NewLine;
+                                    }
+
+                                    found = false;          // Reset found
                                 }
 
-                                found = false;          // Reset found
+                                File.AppendAllText(fileName, toAppend);
+                                MessageBox.Show("Following names are repeated and are discarded : " + Environment.NewLine +
+                                    repeated, "File updated");
                             }
-
-                            File.AppendAllText(fileName, toAppend);
-                            MessageBox.Show("Following names are repeated and are discareded : " + Environment.NewLine +
-                                repeated, "File updated");
                         }
                     }
                 }

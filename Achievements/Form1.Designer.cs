2578fcdc8740dbbfb58ec5d909d3b691a11f511c
Syed Rafay <rafay_syed1@yahoo.com>
2017-06-13 05:25:45 +0500

    Started the achievements project, implemented the basic functionality

---
+++ Achievements/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Achievements
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.achievementType = new System.Windows.Forms.CheckedListBox();
+            this.membersName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.suspectsNames = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Members Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Achievement type :";
+            // 
+            // achievementType
+            // 
+            this.achievementType.CheckOnClick = true;
+            this.achievementType.FormattingEnabled = true;
+            this.achievementType.Items.AddRange(new object[] {
+            "Chainsaw",
+            "Sniper",
+            "Bomb",
+            "Drag Most Wanted",
+            "Helicopter Drag"});
+            this.achievementType.Location = new System.Drawing.Point(138, 57);
+            this.achievementType.Name = "achievementType";
+            this.achievementType.Size = new System.Drawing.Size(195, 79);
+            this.achievementType.TabIndex = 2;
+            // 
+            // membersName
+            // 
+            this.membersName.Location = new System.Drawing.Point(138, 10);
+            this.membersName.Name = "membersName";
+            this.membersName.Size = new System.Drawing.Size(195, 20);
+            this.membersName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Suspect\'s name(s) :";
+            // 
+            // suspectsNames
+            // 
+            this.suspectsNames.Location = new System.Drawing.Point(138, 165);
+            this.suspectsNames.Name = "suspectsNames";
+            this.suspectsNames.Size = new System.Drawing.Size(195, 246);
+            this.suspectsNames.TabIndex = 5;
+            this.suspectsNames.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 50);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Check and Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 559);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.suspectsNames);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.membersName);
+            this.Controls.Add(this.achievementType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox achievementType;
+        private System.Windows.Forms.TextBox membersName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox suspectsNames;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Achievements/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Achievements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "SWAT Achievements Manager v1.0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+
+            if (membersName.TextLength == 0)
+                MessageBox.Show("Please enter the members name in the field", "Error",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                fileName = membersName.Text;
+
+                if (achievementType.CheckedIndices.Count == 0)
+                    MessageBox.Show("Please select the achievement type for the user", "Error",
+                     MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    if (achievementType.CheckedIndices.Count > 1)
+                        MessageBox.Show("You can only select one achievement at a time", "Error",
+                         MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    else
+                    {
+                        if (achievementType.GetItemCheckState(0) == CheckState.Checked)
+                            fileName += @"\Chainsaw.txt";
+                        else if (achievementType.GetItemCheckState(1) == CheckState.Checked)
+                            fileName += @"\Sniper.txt";
+                        else if (achievementType.GetItemCheckState(2) == CheckState.Checked)
+                            fileName += @"\Bomb.txt";
+                        else if (achievementType.GetItemCheckState(3) == CheckState.Checked)
+                            fileName += @"\DragMW.txt";
+                        else if (achievementType.GetItemCheckState(4) == CheckState.Checked)
+                            fileName += @"\HeliDrag.txt";
+
+                        if (!File.Exists(fileName))
+                        {
+                            string buffer = "";
+                            int numOfLines = suspectsNames.Lines.Length;
+                            FileInfo file = new FileInfo(fileName);
+                            file.Directory.Create(); // If the directory already exists, this method does nothing.
+                            for (int i = 0; i < numOfLines; ++i)
+                            {
+                                buffer += suspectsNames.Lines[i];
+                                if (i + 1 != numOfLines)
+                                    buffer += Environment.NewLine;
+                            }
+                            File.WriteAllText(fileName, buffer);
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
